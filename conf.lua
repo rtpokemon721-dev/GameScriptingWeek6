cebd2efe76209758c2b707ff64145fb04d7c1b5a
Ryan Thompson <rtpokemon721@gmail.com>
2025-10-02 11:37:56 -0500

    Create conf.lua

---
+++ conf.lua
@@ -0,0 +1,7 @@
+function love.conf(t)
+    t.title = "My LÖVE Game"        -- The title of the window (default "Untitled")
+    t.window.width = 800            -- Window width (default 800)
+    t.window.height = 600           -- Window height (default 600)
+    t.window.resizable = false      -- Let the window be user-resizable (default false)
+    t.window.vsync = 1              -- Vertical sync mode (default 1)
+end
